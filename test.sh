8a69fb7aaec73050d0b243db0b502596957406a6
Tetsuyuki Kobayashi <tetsu.koba@gmail.com>
2023-07-13 11:56:12 +0900

    Fix test.sh for Linux

--- test.sh
+++ test.sh
@@ -1,9 +1,10 @@
-#!/bin/sh -eux
+#!/bin/bash -eux
 case "$OSTYPE" in
     darwin*) case "$HOSTTYPE" in
 		 arm64) LIBDIR=/opt/homebrew/lib;INCLUDE=/opt/homebrew/include;;
 		 *)	LIBDIR=/usr/local/lib;INCLUDE=/usr/local/include;;
 	     esac ;;
+     *) LIBDIR=/usr/lib;INCLUDE=/usr/include;;
 esac
 
 if [ $# -eq 0 ]; then
